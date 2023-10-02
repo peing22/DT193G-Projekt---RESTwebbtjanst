e9bdf093a9e753a46155e66fe25eb236f94c1669
Petra Ingemarsson <petra.ingemarsson@gmail.com>
2023-10-02 09:09:42 +0200

    Modeller och migrationsfiler

---
+++ database/migrations/2023_10_02_064159_create_plants_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('plants', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('category_id')->unsigned();
+            $table->string('name', 64);
+            $table->longText('description')->nullable();
+            $table->string('image')->nullable();
+            $table->integer('price')->nullable();
+            $table->integer('quantity');
+            $table->timestamps();
+            $table->foreign('category_id')->references('id')->on('categories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('plants');
+    }
+};
